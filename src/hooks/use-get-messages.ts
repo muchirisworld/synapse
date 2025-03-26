cea89a56cb591d8a08987240939f06037466b06c
Muchiri <developedbyrobbie@gmail.com>
2025-03-26 22:09:36 +0300

    feat: Add custom hook useGetMessages for paginated message retrieval

---
+++ src/hooks/use-get-messages.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+import { PaginatedQueryReference, usePaginatedQuery } from "convex/react";
+
+const BATCH_SIZE = 20;
+
+type UseGetMessagesProps = {
+    channelId?: Id<"channels">;
+    conversationId?: Id<"conversations">;
+    parentMessageId?: Id<"messages">;
+};
+
+export const useGetMessages = ({
+    channelId,
+    conversationId,
+    parentMessageId,
+}: UseGetMessagesProps) => {
+
+    const { results, isLoading, loadMore, status } = usePaginatedQuery(
+            api.message.get as PaginatedQueryReference,
+            { channelId, conversationId, parentMessageId },
+            { initialNumItems: BATCH_SIZE }
+        )
+    
+    return {
+        results,
+        isLoading,
+        loadMore: () => loadMore(BATCH_SIZE),
+        status
+    }
+};
